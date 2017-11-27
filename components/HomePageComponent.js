d12f1d2f6f4fc8da8b46d8c3b6000953603ba35d
BrettDixonBCIT <brettdixon1@gmail.com>
2017-11-26 18:10:38 -0800

    Letter generator made.  Added Play and Keep Score button to Home.

--- components/HomePageComponent.js
+++ components/HomePageComponent.js
@@ -17,18 +17,28 @@ export default class HomePageComponent extends Component {
 		header: null
 	};
 
+	startGame(keepScore){
+        const {navigate} = this.props.navigation;
+        let score = [[0,0]];  //both teams start a 0 for the first round.
+        navigate('Generate', {keepScore: keepScore, score: score})
+	}
+
 	render() {		
-		const {navigate} = this.props.navigation;
+
 		return (
 			<View style={styles.bodyContainer}>
 				<StatusBar 
 					hidden={true}
 				/>
-		        <Animatable.Text animation="shake" iterationCount={'infinite'} direction="alternate" style={styles.textStyle}>Bamboozle</Animatable.Text>
-
-		        <TouchableOpacity onPress={() => navigate('Generate')}>
+				<View>
+		        	<Animatable.Text animation="shake" iterationCount={'infinite'} direction="alternate" style={styles.textStyle}>Bamboozle</Animatable.Text>
+				</View>
+		        <TouchableOpacity onPress={() => this.startGame(false)}>
 		        	<Text style={styles.button}>PLAY</Text>
 		        </TouchableOpacity>
+				<TouchableOpacity onPress={() => this.startGame(true)}>
+					<Text style={styles.button}>PLAY AND KEEP SCORE</Text>
+				</TouchableOpacity>
 
 			</View>
 		)
