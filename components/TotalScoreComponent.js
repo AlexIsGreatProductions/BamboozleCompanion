fdf91aa5eb52ab413f4163243c90d5a412a5a43a
BrettDixonBCIT <brettdixon1@gmail.com>
2017-12-02 15:15:39 -0800

    safety push

--- components/TotalScoreComponent.js
+++ components/TotalScoreComponent.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, Button, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import Mailer from 'react-native-mail';
 
+
+const widthArr = [100, 100, 100];
 
 export default class TotalScoreComponent extends Component {
 
     constructor() {
         super();
+        this.header = ['Round', 'Team 1', 'Team 2' ];
+        this.tableData;
+        this.footer;
         this.state = {
             score: null,
+            keepScore: false,
             team1Score: 0,
             team2Score: 0,
             currentRound: 0,
-            team1Gest: 0,
-            team2Gest: 0,
-            team1Verb: 0,
-            team2Verb: 0,
-            team1Cheat: 0,
-            team2Cheat: 0
+            team1Win: false,
+            team2Win: false,
+            gameEnd: false
         }
     }
 
@@ -25,29 +30,57 @@ export default class TotalScoreComponent extends Component {
         title: 'TOTALS SCREEN',
     };
 
-    componentDidMount(){
+    componentWillMount(){
         this.getCurrentScore();
+
+    }
+
+    componentDidMount(){
+        this.evalScore();
     }
 
     getCurrentScore(){
         const {params} = this.props.navigation.state;
         console.log("Params: "+params);
-        console.log("Scores: "+params.score)
+        console.log("Scores: "+params.score);
         this.setState({
-            score: params.score,
-            currentRound: params.round,
+            //score: params.score,
+            //currentRound: params.round,
+            score: [[4,8],[3,6],[10,8],[8,5],[9,5]],
+            currentRound: 5,
             keepScore: params.keepScore
         });
+    }
+
+    evalScore(){
         let temp1 = 0;	//team 1 score
         let temp2 = 0;	//team 2 score
+        console.log('Round '+this.state.currentRound);
+        let rounds = [];
         for(let i=0; i<this.state.currentRound; i++){
+            let roundNum = i+1;
+            let temp = ['Round '+ roundNum, this.state.score[i][0], this.state.score[i][1]];
+            rounds.push(temp);
             temp1 += this.state.score[i][0];
             temp2 += this.state.score[i][1];
         }
+        console.log('Rounds: '+rounds);
+        if((temp1 >= 30) || (temp2 >= 30)) {
+            if (temp1 > temp2) {
+                this.setState({team1Win: true, gameEnd: true});
+            }
+            if (temp1 < temp2) {
+                this.setState({team2Win: false, gameEnd: true});
+            }
+        }
+        let foot = ['Totals', temp1, temp2];
         this.setState({
             team1Score: temp1,
             team2Score: temp2
-        })
+        });
+        this.tableData = rounds;
+        this.footer = foot;
+        console.log('Rows: '+this.tableData);
     }
 
 
@@ -56,18 +89,12 @@ export default class TotalScoreComponent extends Component {
         return (
             <View style={styles.bodyContainer}>
                 <Text>Current Round: {this.state.currentRound}</Text>
-                <Text>Team 1 Current Score: {this.state.team1Score}</Text>
-                <Text>Team 2 Current Score: {this.state.team2Score}</Text>
-                <View>
-                    <Text>Team 1</Text>
-                    <Text>Gestures Clues {this.state.team1Gest}</Text>
-                    <Button
-                        onPress={console.log("WORKS")}
-                        title="+"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                </View>
+                <Table style={styles.table}>
+                    <Row data={this.header} style={styles.head} textStyle={styles.headText} widthArr={widthArr}/>
+
+                    <Rows data={this.tableData} style={styles.list} textStyle={styles.listText} widthArr={widthArr}/>
+                    <Row data={this.footer} style={styles.foot} textStyle={styles.headText} widthArr={widthArr}/>
+                </Table>
             </View>
         )
 
@@ -90,6 +117,12 @@ const styles = StyleSheet.create({
         fontSize: 25,
         color: '#f39c12',
         marginTop: 10
-    }
+    },
+    table: { width: 300, flexDirection: 'column' },
+    head: { backgroundColor: '#333', height: 40 },
+    foot: { backgroundColor: '#333', height: 40},
+    headText: { color: '#fff', textAlign: 'center' },
+    list: { height: 28, backgroundColor: '#f0f0f0' },
+    listText: { textAlign: 'right', marginRight: 6 }
 
 });
